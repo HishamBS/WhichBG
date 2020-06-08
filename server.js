8e8cab67a57286342affc8ee0378fde93694a936
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2020-06-08 13:44:24 +0300

    Deploy ready v1 - added path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 require("dotenv/config");
 mongoose.set("useCreateIndex", true);
 //DB Connection
@@ -14,19 +15,20 @@ mongoose.connect(
   }
 );
 
+server.use(cors());
+
 server.use("/admin", require("./routes/admin.js"));
 
-server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
-// server.use(express.static(path.join(__dirname, "build")));
+server.use(express.static(path.join(__dirname, "build")));
 
 //Routes
 server.use("/api/v1/users", require("./routes/users.js"));
 server.use("/api/v1/posts", require("./routes/posts.js"));
-// server.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+server.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
